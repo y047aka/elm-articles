aeea466ac085f5d3fd6355b48eba2a2b7edd8ff4
Yoshitaka Totsuka <yoshitaka.totsuka@siiibo.com>
2020-07-16 16:20:29 +0900

    Load JSON and display on sortable table

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,7 +1,12 @@
 module Main exposing (main)
 
 import Browser exposing (Document)
-import Html exposing (main_, text)
+import Html exposing (h1, input, main_, section, text)
+import Html.Attributes exposing (class, placeholder)
+import Html.Events exposing (onInput)
+import Http
+import Json.Decode as Decode exposing (Decoder, string)
+import Table
 
 
 main : Program () Model Msg
@@ -19,14 +24,46 @@ main =
 
 
 type alias Model =
-    {}
+    { articles : List Article
+    , tableState : Table.State
+    , query : String
+    }
+
+
+type alias Article =
+    { title : String
+    , author : String
+    , siteName : String
+    , url : String
+    , language : String
+    , targetVersion : String
+    , tags : List String
+    }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( {}
-    , Cmd.none
+    ( { articles = []
+      , tableState = Table.initialSort "Year"
+      , query = ""
+      }
+    , Http.get
+        { url = "articles.json"
+        , expect = Http.expectJson Loaded (Decode.list articleDecoder)
+        }
     )
+
+
+articleDecoder : Decoder Article
+articleDecoder =
+    Decode.map7 Article
+        (Decode.field "title" Decode.string)
+        (Decode.field "author" Decode.string)
+        (Decode.field "siteName" Decode.string)
+        (Decode.field "url" Decode.string)
+        (Decode.field "language" Decode.string)
+        (Decode.field "targetVersion" Decode.string)
+        (Decode.field "tags" (Decode.list Decode.string))
 
 
 
@@ -34,14 +71,25 @@ init _ =
 
 
 type Msg
-    = NoOp
+    = Loaded (Result Http.Error (List Article))
+    | SetQuery String
+    | SetTableState Table.State
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
+        Loaded (Ok articles) ->
+            ( { model | articles = articles }, Cmd.none )
+
+        Loaded (Err _) ->
             ( model, Cmd.none )
+
+        SetQuery newQuery ->
+            ( { model | query = newQuery }, Cmd.none )
+
+        SetTableState newState ->
+            ( { model | tableState = newState }, Cmd.none )
 
 
 
@@ -49,9 +97,38 @@ update msg model =
 
 
 view : Model -> Document Msg
-view _ =
-    { title = ""
+view model =
+    { title = "Elm Articles"
     , body =
-        [ main_ [] [ text "elm-supercell" ]
+        let
+            lowerQuery =
+                String.toLower model.query
+
+            acceptablePeople =
+                List.filter (String.contains lowerQuery << String.toLower << .title) model.articles
+        in
+        [ main_ [ class "ui container" ]
+            [ section [ class "ui vertical stripe segment" ]
+                [ h1 [] [ text "Elm Articles" ]
+                , input [ placeholder "Search by Name", onInput SetQuery ] []
+                , Table.view config model.tableState acceptablePeople
+                ]
+            ]
         ]
     }
+
+
+config : Table.Config Article Msg
+config =
+    Table.config
+        { toId = .title
+        , toMsg = SetTableState
+        , columns =
+            [ Table.stringColumn "Target" .targetVersion
+            , Table.stringColumn "Language" .language
+            , Table.stringColumn "Title" .title
+            , Table.stringColumn "Site" .siteName
+            , Table.stringColumn "URL" .url
+            , Table.stringColumn "Tags" (.tags >> String.join ", ")
+            ]
+        }
