702246c21d15c97420b2f89d44322087e52267d4
Yoshitaka Totsuka <yoshitaka.totsuka@siiibo.com>
2020-09-05 09:53:41 +0900

    Simple site-footer

--- src/Shared.elm
+++ src/Shared.elm
@@ -75,7 +75,7 @@ view { page, toMsg } model =
     , body =
         [ Html.map toMsg <| siteHeader model
         , main_ [ class "ui main container" ] page.body
-        , Html.map toMsg <| siteFooter
+        , siteFooter
         ]
     }
 
@@ -98,98 +98,9 @@ siteHeader { selectedLanguages } =
         ]
 
 
-siteFooter : Html Msg
+siteFooter : Html msg
 siteFooter =
-    let
-        column { parent, children } =
-            div [ class "three wide column" ]
-                [ h1
-                    [ class "ui small inverted header"
-
-                    -- , onClick (SelectTag parent)
-                    ]
-                    [ text (wordToJapanese parent) ]
-                , div [ class "ui inverted link list" ] <|
-                    List.map
-                        (\t ->
-                            div
-                                [ class "item"
-
-                                -- , onClick (SelectTag t)
-                                ]
-                                [ text (wordToJapanese t) ]
-                        )
-                        children
-                ]
-    in
     footer [ class "ui inverted vertical footer segment" ]
-        [ div [ class "ui center aligned container" ]
-            [ div [ class "ui inverted divided grid" ] <|
-                List.map column
-                    [ { parent = "Types", children = [ "Type Aliases", "Custom Types", "Maybe", "Result" ] }
-                    , { parent = "Core", children = [ "HTTP", "JSON", "Random", "Time", "Task" ] }
-                    , { parent = "Test", children = [ "TDD", "elm-test", "elm-verify-examples" ] }
-                    , { parent = "Dev Environment", children = [ "Ellie", "elm reactor", "Parcel", "webpack", "VSCode" ] }
-                    ]
-                    ++ [ div [ class "four wide column" ]
-                            [ h1 [ class "ui small inverted header" ] [ text "elm-articles" ]
-                            , p [] [ text "© 2020 y047aka" ]
-                            ]
-                       ]
-            ]
+        [ div [ class "ui right aligned container" ]
+            [ p [] [ text "© 2020 y047aka" ] ]
         ]
-
-
-wordToJapanese : String -> String
-wordToJapanese englishWord =
-    case englishWord of
-        "Algorithm" ->
-            "アルゴリズム"
-
-        "Custom Types" ->
-            "カスタム型"
-
-        "Dev Environment" ->
-            "開発環境"
-
-        "Environment Setup" ->
-            "環境構築"
-
-        "Examples" ->
-            "作例"
-
-        "Functional Programming" ->
-            "関数型プログラミング"
-
-        "Modules" ->
-            "モジュール"
-
-        "Pattern Matching" ->
-            "パターンマッチ"
-
-        "Personal Log" ->
-            "私的記録"
-
-        "Poème" ->
-            "ポエム"
-
-        "Static Site Generator" ->
-            "静的サイトジェネレーター"
-
-        "Study Log" ->
-            "学習記録"
-
-        "Test" ->
-            "テスト"
-
-        "To-Do List App" ->
-            "ToDoリスト"
-
-        "Types" ->
-            "型"
-
-        "Type Aliases" ->
-            "型エイリアス"
-
-        _ ->
-            englishWord
